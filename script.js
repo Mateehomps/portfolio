5654460ea9d23f803fd0a90ccf19020d8903b74f
Mateo Homps <hompsmateo@gmail.com>
2025-04-09 20:18:34 -0300

    Descripción del cambio (ej: fix CSS path)

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-<script src="https://kit.fontawesome.com/yourkit.js" crossorigin="anonymous"></script>
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('https://api.countapi.xyz/update/mateehomps/portfolio/?amount=1')
+      .then(res => res.json())
+      .then(data => {
+        document.getElementById('visits').innerText = data.value;
+      });
+  });
+
 fetch('https://api.countapi.xyz/get/mateehomps/portfolio/?amount=1')
     .then(res => res.json())
     .then(data => {
       document.getElementById('visits').innerText = data.value;
     });
+   
+      
