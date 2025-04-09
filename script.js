e662d7848533465b2c2f6907912240d91ca2a9e5
Mateo Homps <hompsmateo@gmail.com>
2025-04-09 20:21:01 -0300

    Descripción del cambio (ej: fix CSS path)

--- script.js
+++ script.js
@@ -6,10 +6,5 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   });
 
-fetch('https://api.countapi.xyz/get/mateehomps/portfolio/?amount=1')
-    .then(res => res.json())
-    .then(data => {
-      document.getElementById('visits').innerText = data.value;
-    });
    
       
